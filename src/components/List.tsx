e2b23e911bd5f50e1ecf099f946fb87c65a0c3f3
Marcin <marcin.obiedz@gmail.com>
2019-03-27 21:21:30 +0100

    fetching data

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,13 +1,32 @@
 import React, {Component} from "react";
 import map from "lodash-es/map";
-import {Data} from "../data/people";
 
 type ListProps = {};
+type ListState = {
+	names: string[]
+}
 
-export class List extends Component<ListProps> {
-    render(): React.ReactNode {
-        return <ul>
-            {map(Data.getData(), (name: string) => <li><h1>{name}</h1></li>)}
-        </ul>;
-    }
+export class List extends Component<ListProps, ListState> {
+	state = {
+		names: []
+	};
+
+	componentDidMount(): void {
+		fetch("http://localhost:9001")
+			.then(function (response) {
+				return response.json();
+			})
+			.then(({response}) => {
+				this.setState({
+					names: response.map(({forename}) => forename)
+				})
+			});
+
+	}
+
+	render(): React.ReactNode {
+		return <ul>
+			{map(this.state.names, (name: string) => <li><h1>{name}</h1></li>)}
+		</ul>;
+	}
 }
--- src/data/people.ts
+++
@@ -1,7 +0,0 @@
-const PEOPLES = [
-    "Marcin", "Piotr", "Monika", "Zdzichu", "Radek", "Dupa", "Ktos"
-]
-
-export namespace Data {
-    export const getData = () => PEOPLES
-}
